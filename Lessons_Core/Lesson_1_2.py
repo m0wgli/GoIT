efe41f2d5e60c9fc01d8c0bc827c1f63c6b4b0f2
m0wgli <rodiontiutiunyk@gmail.com>
2023-02-03 02:01:08 +0200

    Create Lesson_1_2.py

---
+++ Lessons_Core/Lesson_1_2.py
@@ -0,0 +1,11 @@
+# ax + b = 0
+# a, b = float(input("Enter number a: ")), float(input("Enter number b: "))
+# x = -b / a
+# print("x equal: ", x)
+
+#input seconds output hours, minutes and seconds
+# seconds = float(input("Enter seconds: "))
+# hours = seconds // 3600
+# minutes = (seconds % 3600) // 60
+# seconds = (seconds % 3600) % 60
+# print(f"Hours {hours}, minutes {minutes}, seconds {seconds}")
